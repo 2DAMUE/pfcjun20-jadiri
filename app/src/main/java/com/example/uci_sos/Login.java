1dd246cec19ca245ead0e3c5582dd01586d9156c
Ricardo Bordería Pi <rychyy999@gmail.com>
2020-05-21 13:23:50 +0200

    javadoc login y registro

--- app/src/main/java/com/example/uci_sos/Login.java
+++ app/src/main/java/com/example/uci_sos/Login.java
@@ -6,8 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+/**
+ * Login de la aplicación. Desde el puedes navegar al registro y a la página principal de la aplicación.
+ *
+ * @see Registro
+ */
 public class Login extends Activity implements View.OnClickListener {
 
+    /**
+     * Label que lleva al registro
+     *
+     * @see Registro
+     */
     TextView lblRegistro;
 
     @Override
@@ -18,10 +28,16 @@ public class Login extends Activity implements View.OnClickListener {
         cargarListeners();
     }
 
+    /**
+     * Carga los listeners de los elementos de la ventana
+     */
     private void cargarListeners() {
         lblRegistro.setOnClickListener(this);
     }
 
+    /**
+     * Carga los elemento de la ventana
+     */
     private void cargarVista() {
         lblRegistro = findViewById(R.id.lblRegistro);
     }
@@ -36,6 +52,11 @@ public class Login extends Activity implements View.OnClickListener {
         }
     }
 
+    /**
+     * Lleva al registro
+     *
+     * @see Registro
+     */
     private void toRegistro() {
         Intent inte = new Intent(this.getApplicationContext(), Registro.class);
         startActivity(inte);
--- app/src/main/java/com/example/uci_sos/Registro.java
+++ app/src/main/java/com/example/uci_sos/Registro.java
@@ -3,15 +3,9 @@ package com.example.uci_sos;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.RotateAnimation;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -22,8 +16,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Registro de la aplicación. En él creas tu usuario para l aaplicación
+ */
 public class Registro extends Activity {
 
+    /**
+     * Spinner con la lista de hospitales registrados en la aplicación
+     */
     private Spinner spinHospital;
 
     @Override
@@ -34,14 +34,25 @@ public class Registro extends Activity {
         cargarListeners();
     }
 
+    /**
+     * Carga los listeners de los elementos de la ventana
+     */
     private void cargarListeners() {
     }
 
+    /**
+     * Carga los elemento de la ventana
+     */
     private void cargarVista() {
         spinHospital = findViewById(R.id.spinnerRegistro);
         cargarAdapter();
     }
 
+    /**
+     * Crea y carga la lista de hospitales en el spinner
+     *
+     * @see Registro#spinHospital
+     */
     private void cargarAdapter() {
         String[] nombres = new String[]{"Selecciona un hospital", "Puerta de Hierro", "Gregorio Marañón", "Montepríncipe"};
         List<String> listaNombres = new ArrayList<>(Arrays.asList(nombres));
