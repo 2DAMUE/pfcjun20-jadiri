c0146a51199373da65a24b44e6431fc895750f2c
Ricardo Bordería Pi <rychyy999@gmail.com>
2020-05-21 13:30:56 +0200

    merge splash y javadoc splash

--- app/src/main/java/com/example/uci_sos/Splash.java
+++ app/src/main/java/com/example/uci_sos/Splash.java
@@ -1,17 +1,18 @@
 package com.example.uci_sos;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.ActionMode;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.TextView;
 
+/**
+ * Splash de la aplicación. Te lleva al login
+ *
+ * @see Login
+ */
 public class Splash extends Activity {
 
     @Override
@@ -24,7 +25,6 @@ public class Splash extends Activity {
         //TextView colaboracion = findViewById(R.id.colabo);
         Animation zoomin = AnimationUtils.loadAnimation(this, R.anim.animacionlogo);
         Animation pulso = AnimationUtils.loadAnimation(this, R.anim.latidos);
-        Animation aparece = AnimationUtils.loadAnimation(this, R.anim.animacionlogouem);
         Animation facein = AnimationUtils.loadAnimation(this, R.anim.animaciontexto);
         logosos.startAnimation(zoomin);
         logosos.startAnimation(pulso);
@@ -33,7 +33,11 @@ public class Splash extends Activity {
         //colaboracion.startAnimation(facein);
         start();
     }
-    private void start(){
+
+    /**
+     * Inicia la animación
+     */
+    private void start() {
         Handler han = new Handler();
         han.postDelayed(new Runnable() {
             @Override
@@ -43,6 +47,5 @@ public class Splash extends Activity {
                 finish();
             }
         }, 4000);
-
     }
 }
