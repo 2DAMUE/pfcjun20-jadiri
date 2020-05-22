833e4531bb38093fc1cd1963266cbdc3a0982460
Ricardo Bordería Pi <rychyy999@gmail.com>
2020-05-22 11:57:51 +0200

    commit antes de merge con JavierReserva

--- app/src/main/java/com/example/uci_sos/Reservar.java
+++ app/src/main/java/com/example/uci_sos/Reservar.java
@@ -3,6 +3,7 @@ package com.example.uci_sos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Reservar extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class Reservar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
