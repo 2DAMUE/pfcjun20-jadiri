44658b157d639355e450cead612523cfd2225af8
JavierFer98 <pedo619+caca>
2020-05-22 13:48:50 +0200

    Otro cambio

--- app/src/main/java/com/example/uci_sos/Datos.java
+++ app/src/main/java/com/example/uci_sos/Datos.java
@@ -3,6 +3,7 @@ package com.example.uci_sos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Datos extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class Datos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos);
+        setTitle("Datos");
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    }
