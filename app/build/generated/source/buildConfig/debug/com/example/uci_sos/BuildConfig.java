9d206f57c966ceeb873da64b0e2d8f67c87881fb
Ricardo Bordería Pi <rychyy999@gmail.com>
2020-05-25 14:18:32 +0200

    añadido librerías firebase

--- app/build/generated/source/buildConfig/debug/com/example/uci_sos/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/uci_sos/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.uci_sos;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.ejemplorecyclerview";
+  public static final String APPLICATION_ID = "com.example.uci_sos";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
