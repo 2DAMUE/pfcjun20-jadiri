ad1708622a6f689ae42c3263bd0eeabc3d03cd8b
JavierFer98 <pedo619+caca>
2020-05-25 12:53:56 +0200

    BUSCAAAAR

--- app/src/main/java/com/example/uci_sos/Hospital.java
+++ app/src/main/java/com/example/uci_sos/Hospital.java
@@ -1,59 +1,101 @@
 package com.example.uci_sos;
 
-import java.util.List;
-
+/**
+ * Contiene los datos de un hospital
+ */
 public class Hospital {
-    private int id;
-    private String nombre;
-    private double latitud;
-    private double longitud;
-    private List<Camas> lista_camas;
 
-    public Hospital(int id, String nombre, double latitud, double longitud, List<Camas> lista_camas) {
-        this.id = id;
+    /**
+     * Nombre del hospital
+     */
+    String nombre;
+
+    /**
+     * Número de camas de UCI
+     */
+    int camasUCI;
+    /**
+     * Número de camas de planta
+     */
+    int camasPlanta;
+    /**
+     * Número de camas de urgencias
+     */
+    int camasUrgencias;
+    /**
+     * Número de camas disponibles para derivaciones de otros hospitales
+     */
+    int camasDisponibles;
+
+    /**
+     * Constructor de la clase
+     *
+     * @param nombre           nombre del hospital
+     * @param camasUCI         número de camas de UCI
+     * @param camasPlanta      número de camas de planta
+     * @param camasUrgencias   número de camas de urgencias
+     * @param camasDisponibles número de camas disponibles para derivaciones de otros hospitales
+     * @see Hospital#nombre
+     * @see Hospital#camasUCI
+     * @see Hospital#camasPlanta
+     * @see Hospital#camasUrgencias
+     * @see Hospital#camasDisponibles
+     */
+    public Hospital(String nombre, int camasUCI, int camasPlanta, int camasUrgencias, int camasDisponibles) {
         this.nombre = nombre;
-        this.latitud = latitud;
-        this.longitud = longitud;
-        this.lista_camas = lista_camas;
+        this.camasUCI = camasUCI;
+        this.camasPlanta = camasPlanta;
+        this.camasUrgencias = camasUrgencias;
+        this.camasDisponibles = camasDisponibles;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    /**
+     * Getter del nombre del hospital
+     *
+     * @return nombre del hospital
+     * @see Hospital#nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    /**
+     * Getter del número de camas de UCI
+     *
+     * @return número de camas de UCI
+     * @see Hospital#camasUCI
+     */
+    public int getCamasUCI() {
+        return camasUCI;
     }
 
-    public double getLatitud() {
-        return latitud;
+    /**
+     * Getter del número de camas de planta
+     *
+     * @return número de camas de planta
+     * @see Hospital#camasPlanta
+     */
+    public int getCamasPlanta() {
+        return camasPlanta;
     }
 
-    public void setLatitud(double latitud) {
-        this.latitud = latitud;
+    /**
+     * Getter del número de camas de urgencias
+     *
+     * @return número de camas de urgencias
+     * @see Hospital#camasUrgencias
+     */
+    public int getCamasUrgencias() {
+        return camasUrgencias;
     }
 
-    public double getLongitud() {
-        return longitud;
-    }
-
-    public void setLongitud(double longitud) {
-        this.longitud = longitud;
-    }
-
-    public List<Camas> getLista_camas() {
-        return lista_camas;
-    }
-
-    public void setLista_camas(List<Camas> lista_camas) {
-        this.lista_camas = lista_camas;
+    /**
+     * Geter del número de camas disponibles para derivaciones de otros hospitales
+     *
+     * @return número de camas disponibles para derivaciones de otros hospitales
+     * @see Hospital#camasDisponibles
+     */
+    public int getCamasDisponibles() {
+        return camasDisponibles;
     }
 }
