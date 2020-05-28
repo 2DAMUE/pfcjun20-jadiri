f7d9ec8389ea34c85f195d94e989f391b4fdeb10
JavierFer98 <pedo619+caca>
2020-05-28 14:12:47 +0200

    GRAFIKITO BONIKO

--- app/src/main/java/com/example/uci_sos/MiHospital.java
+++ app/src/main/java/com/example/uci_sos/MiHospital.java
@@ -3,6 +3,7 @@ package com.example.uci_sos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -14,6 +15,17 @@ import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Pie;*/
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,12 +33,17 @@ public class MiHospital extends AppCompatActivity implements View.OnClickListene
     LinearLayout reservar;
     LinearLayout miscamas;
     LinearLayout mihospital;
+    AnyChartView graficocircular;
+    String[]estados={"Libres","Ocupadas","No disponibles"};
+    int[]numeros={250,102,50};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mi_hospital);
+        setContentView(R.layout.activityhospital);
+        setupPieChart();
         cargarVista();
         cargarListeners();
+
         /*Pie pie = AnyChart.pie();
 
         List<DataEntry> data = new ArrayList<>();
@@ -38,6 +55,21 @@ public class MiHospital extends AppCompatActivity implements View.OnClickListene
 
         AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
         anyChartView.setChart(pie);*/
+    }
+
+    private void setupPieChart() {
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for(int i=0; i < estados.length; i++){
+            pieEntries.add(new PieEntry(numeros[i], estados[i]));
+        }
+        PieDataSet dataSet = new PieDataSet(pieEntries, "Estado??");
+        PieData data = new PieData(dataSet);
+        dataSet.setColors(getResources().getColor(R.color.colorLibres), getResources().getColor(R.color.colorOcupadas), getResources().getColor(R.color.colorNoDisponible));
+
+        PieChart chart = (PieChart) findViewById(R.id.grafico_circulo);
+        chart.setData(data);
+        chart.invalidate();
+        chart.animateXY(1000,1000);
     }
 
     private void cargarListeners() {
@@ -52,6 +84,7 @@ public class MiHospital extends AppCompatActivity implements View.OnClickListene
         reservar = findViewById(R.id.btnReservarHospital);
         miscamas = findViewById(R.id.btnMisCamasHospital);
         mihospital = findViewById(R.id.btnHospitalHospital);
+        //graficocircular = findViewById(R.id.grafico_circulo);
     }
 
     @Override
@@ -98,4 +131,5 @@ public class MiHospital extends AppCompatActivity implements View.OnClickListene
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+
 }
