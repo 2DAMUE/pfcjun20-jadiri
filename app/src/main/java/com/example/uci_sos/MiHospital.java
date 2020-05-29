6499edff158c3f5f2dce4eae03285c991e731eff
JavierFer98 <pedo619+caca>
2020-05-29 14:07:42 +0200

    PRUEBADEGRAFICODEBARRAS

--- app/src/main/java/com/example/uci_sos/MiHospital.java
+++ app/src/main/java/com/example/uci_sos/MiHospital.java
@@ -1,90 +1,59 @@
 package com.example.uci_sos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
-/*import com.anychart.AnyChart;
-import com.anychart.AnyChartView;
-import com.anychart.chart.common.dataentry.DataEntry;
-import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Pie;*/
+import com.example.uci_sos.modelo.entidad.Hospital;
 
-import com.anychart.AnyChart;
-import com.anychart.AnyChartView;
-import com.anychart.chart.common.dataentry.DataEntry;
-import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Pie;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MiHospital extends AppCompatActivity implements View.OnClickListener{
+/**
+ * Muestra los datos del Hospital en el que está registrado el usuario
+ *
+ * @see Hospital
+ */
+public class MiHospital extends AppCompatActivity implements View.OnClickListener {
+
+    /**
+     * Botón que lleva a la ventana de Reservar
+     *
+     * @see Reservar
+     */
     LinearLayout reservar;
+    /**
+     * Botón que lleva a la ventana de MisCamas
+     *
+     * @see MisCamas
+     */
     LinearLayout miscamas;
-    LinearLayout mihospital;
-    AnyChartView graficocircular;
-    String[]estados={"Libres","Ocupadas","No disponibles"};
-    int[]numeros={250,102,50};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activityhospital);
-        setupPieChart();
         cargarVista();
         cargarListeners();
-
-        /*Pie pie = AnyChart.pie();
-
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("John", 10000));
-        data.add(new ValueDataEntry("Jake", 12000));
-        data.add(new ValueDataEntry("Peter", 18000));
-
-        pie.data(data);
-
-        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
-        anyChartView.setChart(pie);*/
+        setupPieChart();
     }
 
-    private void setupPieChart() {
-        List<PieEntry> pieEntries = new ArrayList<>();
-        for(int i=0; i < estados.length; i++){
-            pieEntries.add(new PieEntry(numeros[i], estados[i]));
-        }
-        PieDataSet dataSet = new PieDataSet(pieEntries, "Estado??");
-        PieData data = new PieData(dataSet);
-        dataSet.setColors(getResources().getColor(R.color.colorLibres), getResources().getColor(R.color.colorOcupadas), getResources().getColor(R.color.colorNoDisponible));
-
-        PieChart chart = (PieChart) findViewById(R.id.grafico_circulo);
-        chart.setData(data);
-        chart.invalidate();
-        chart.animateXY(1000,1000);
-    }
-
-    private void cargarListeners() {
-        reservar.setOnClickListener(this);
-        miscamas.setOnClickListener(this);
-        mihospital.setOnClickListener(this);
-
-    }
-
-
-    private void cargarVista() {
-        reservar = findViewById(R.id.btnReservarHospital);
-        miscamas = findViewById(R.id.btnMisCamasHospital);
-        mihospital = findViewById(R.id.btnHospitalHospital);
-        //graficocircular = findViewById(R.id.grafico_circulo);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
     @Override
@@ -101,35 +70,100 @@ public class MiHospital extends AppCompatActivity implements View.OnClickListene
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.itLogout:
+                FirebaseAuth.getInstance().signOut();
+                toLogin();
+                break;
+            case R.id.itConfig:
+                toConfig();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-    private void toReservar() {
-        Intent inte = new Intent(this.getApplicationContext(), Reservar.class);
+    /**
+     * Carga los datos de los hospitales en el gráfico
+     *
+     * @see Hospital
+     */
+    private void setupPieChart() {
+        String[] estados = {"Libres", "Ocupadas", "No disponibles"};
+        int[] numeros = {250, 102, 50};
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i = 0; i < estados.length; i++) {
+            pieEntries.add(new PieEntry(numeros[i], estados[i]));
+        }
+        PieDataSet dataSet = new PieDataSet(pieEntries, "Estado??");
+        PieData data = new PieData(dataSet);
+        dataSet.setColors(getResources().getColor(R.color.colorLibres), getResources().getColor(R.color.colorOcupadas), getResources().getColor(R.color.colorNoDisponible));
+
+        PieChart chart = findViewById(R.id.grafico_circulo);
+        chart.setData(data);
+        chart.invalidate();
+        chart.animateXY(1000, 1000);
+    }
+
+    /**
+     * Lleva a la ventana de configuración del hospital
+     *
+     * @see Datos
+     */
+    private void toConfig() {
+        Intent inte = new Intent(this, Datos.class);
         startActivity(inte);
     }
 
+    /**
+     * Lleva al login
+     *
+     * @see Login
+     */
+    private void toLogin() {
+        Intent inte = new Intent(this, Login.class);
+        inte.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        finish();
+        startActivity(inte);
+    }
+
+    /**
+     * Lleva a la ventana de Reservar
+     *
+     * @see Reservar
+     */
+    private void toReservar() {
+        Intent inte = new Intent(this.getApplicationContext(), Reservar.class);
+        finish();
+        startActivity(inte);
+    }
+
+    /**
+     * Lleva a la ventana de MisCamas
+     *
+     * @see MisCamas
+     */
     private void toCamas() {
         Intent inte = new Intent(this.getApplicationContext(), MisCamas.class);
         finish();
         startActivity(inte);
     }
 
-    private void toPlanta() {
-        Intent inte = new Intent(this.getApplicationContext(), ConfigPlanta.class);
-        finish();
-        startActivity(inte);
+    /**
+     * Carga los listeners de los elementos de la ventana
+     */
+    private void cargarListeners() {
+        reservar.setOnClickListener(this);
+        miscamas.setOnClickListener(this);
     }
 
-    private void toHospital() {
-        Intent inte = new Intent(this.getApplicationContext(), Login.class);
-        finish();
-        startActivity(inte);
+    /**
+     * Carga los elementos de la ventana
+     */
+    private void cargarVista() {
+        reservar = findViewById(R.id.btnReservarHospital);
+        miscamas = findViewById(R.id.btnMisCamasHospital);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
 }
---
+++ app/src/main/java/com/example/uci_sos/MiHospitall.java
@@ -0,0 +1,160 @@
+package com.example.uci_sos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MiHospitall extends AppCompatActivity implements View.OnClickListener {
+    LinearLayout reservar;
+    LinearLayout miscamas;
+    LinearLayout mihospital;
+    BarChart barChart;
+    ArrayList<String> estados;
+    float[] numeros = {250, 102, 50, 230, 30, 2, 56, 43, 0};
+    ArrayList<BarEntry> barEntryArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_hospitall);
+        int colores[] = new int[]{getResources().getColor(R.color.colorOcupadas), getResources().getColor(R.color.colorNoDisponible), getResources().getColor(R.color.colorLibres)};
+        barChart = (BarChart) findViewById(R.id.graficobarras);
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setMaxVisibleValueCount(50);
+        barChart.setPinchZoom(false);
+        barChart.setDrawGridBackground(true);
+        String[] listas = {"UCI", "PLANTA", "URGENCIAS"};
+        ArrayList<BarEntry> barEntries1 = new ArrayList<>();
+        barEntries1.add(new BarEntry(1, 40f));
+        barEntries1.add(new BarEntry(2, 43f));
+        barEntries1.add(new BarEntry(3, 32f));
+        ArrayList<BarEntry> barEntries2 = new ArrayList<>();
+        barEntries2.add(new BarEntry(1, 12f));
+        barEntries2.add(new BarEntry(2, 16f));
+        barEntries2.add(new BarEntry(3, 13f));
+        ArrayList<BarEntry> barEntries3 = new ArrayList<>();
+        barEntries3.add(new BarEntry(1, 4f));
+        barEntries3.add(new BarEntry(2, 6f));
+        barEntries3.add(new BarEntry(3, 2f));
+
+
+        BarDataSet barDataSet1 = new BarDataSet(barEntries1, "");
+        barDataSet1.setColors(colores);
+        BarDataSet barDataSet2 = new BarDataSet(barEntries2, "");
+        barDataSet2.setColors(colores);
+        BarDataSet barDataSet3 = new BarDataSet(barEntries3, "");
+        barDataSet3.setColors(colores);
+        Description description = new Description();
+        description.setText("");
+        barChart.setDescription(description);
+        BarData data = new BarData(barDataSet1, barDataSet2, barDataSet3);
+
+        float groupSpace = 0.1f;
+        float barSpace = 0.02f;
+        float barWidth = 0.23f;
+        barChart.setData(data);
+        barChart.getLegend().setEnabled(false);
+
+        data.setBarWidth(barWidth);
+        barChart.groupBars(1, groupSpace, barSpace);
+        data.setDrawValues(false);
+        String[] camillas = new String[]{"", "Planta", "Urgencias", "UCI"};
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(camillas));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1f);
+        xAxis.setCenterAxisLabels(false);
+        xAxis.setAxisMinimum(1);
+        //xAxis.setLabelRotationAngle(270);
+        barChart.animateY(1000);
+        barChart.invalidate();
+        xAxis.setLabelCount(camillas.length);
+        cargarVista();
+        cargarListeners();
+    }
+
+    private void cargarListeners() {
+        reservar.setOnClickListener(this);
+        miscamas.setOnClickListener(this);
+        mihospital.setOnClickListener(this);
+
+    }
+
+
+    private void cargarVista() {
+        reservar = findViewById(R.id.btnReservarHospital);
+        miscamas = findViewById(R.id.btnMisCamasHospital);
+        mihospital = findViewById(R.id.btnHospitalHospital);
+        //graficocircular = findViewById(R.id.grafico_circulo);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btnReservarHospital:
+                toReservar();
+                break;
+            case R.id.btnMisCamasHospital:
+                toCamas();
+                break;
+
+        }
+    }
+
+
+    private void toReservar() {
+        Intent inte = new Intent(this.getApplicationContext(), Reservar.class);
+        startActivity(inte);
+    }
+
+    private void toCamas() {
+        Intent inte = new Intent(this.getApplicationContext(), MisCamas.class);
+        finish();
+        startActivity(inte);
+    }
+
+    private void toPlanta() {
+        Intent inte = new Intent(this.getApplicationContext(), ConfigPlanta.class);
+        finish();
+        startActivity(inte);
+    }
+
+    private void toHospital() {
+        Intent inte = new Intent(this.getApplicationContext(), Login.class);
+        finish();
+        startActivity(inte);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+}
